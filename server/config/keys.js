3b463f1628b53f99c411dec942eee6363b1dbcf9
Adam Lengyel <lengyel.adam95@gmail.com>
2019-04-16 21:42:00 +0200

    keys.js deleted

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: '162509998478-ehd4lp7nobiefd91npeb6rf4aq8rllt7.apps.googleusercontent.com',
-        clientSecret: 'FMWu-1gS6h7CyUIRAKzFDB52'
-    }
-};
